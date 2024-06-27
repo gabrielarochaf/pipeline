7f1436d8c9d1c4b9dac6de226da7da03fc658762
gabrielarochaf <oliveiragabriela278@gmail.com>
2024-06-26 23:06:04 -0300

    teste

--- Backend/backend/settings/__init__.py
+++ Backend/backend/settings/__init__.py
@@ -1,5 +1,9 @@
 from decouple import config
 
+
+DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
+
 environment = config('DJANGO_SETTINGS_MODULE', default='backend.settings.devolpment')
 
 if environment == 'backend.settings.production':
